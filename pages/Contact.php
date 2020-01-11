9ee9fe692b3f06c4f08e5cf82e09c2c75e3c3566
faouzidev <faouzi_jarray@yahoo.fr>
2020-01-11 14:24:30 -0500

    FirstCommitAujourdhui

--- pages/Contact.php
+++ pages/Contact.php
@@ -1,9 +1,4 @@
 <table>
 </table>
-<label>Email tous cours</label>
 <h1>Contact</h1>
-<p>2 emme essai Modification 2 </p>
-<label for="">Mon adresse email : test@</label>
-
-<p>2 emme essai Modification 1 </p>
-<input type="hidden" value="id_entity">
+<p>Email : MonAdresseEmail</p>
